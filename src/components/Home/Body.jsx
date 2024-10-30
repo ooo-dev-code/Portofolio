81b12a4a92c851d03136c018f8d312d19d164aa3
ooo-dev-code <183236447+ooo-dev-code@users.noreply.github.com>
2024-10-31 00:43:18 +0100

    first commit

--- src/components/Home/Body.jsx
+++ src/components/Home/Body.jsx
@@ -4,8 +4,7 @@ import styled from 'styled-components'
 function Body() {
   return (
     <div>
-
-        <Container className='container'>
+        <Container1 className='container1'>
             <Title_container className='Title_container'>
                 <Title className='Title'>
                     <u>My First Portofolio</u>
@@ -15,12 +14,35 @@ function Body() {
                     <p className='sub'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, consectetur. Maiores autem nemo expedita soluta error aliquid suscipit deserunt pariatur tempore, quo excepturi. Impedit ullam explicabo qui laboriosam vitae consectetur.</p>
                 </Title>
             </Title_container>
-        </Container>
+        </Container1>
+        <Container1 className='container2'>
+            
+            <div className="transition_block"></div>
+            <div className="transition_block"></div>
+            <div className="transition_block"></div>
+            <div className="transition_block"></div>
+            <div className="transition_block"></div>
+            <div className="transition_block"></div>
+            <div className="transition_block"></div>
+            <div className="transition_block"></div>
+            <div className="transition_block"></div>
+            <div className="transition_block"></div>
+            <div className="transition_block"></div>
+            <div className="transition_block"></div>
+            <div className="transition_block"></div>
+
+            
+
+            <div  className='icone1'><img src="src\assets\js_icon.png" alt=""/></div>
+            <img src="src\assets\py_icon.png" alt="" className='icone2'/>
+
+            <div className="text_box"></div>
+        </Container1>
     </div>
   )
 }
 
-const Container = styled.div``;
+const Container1 = styled.div``;
 
 const Title = styled.div``;
 
